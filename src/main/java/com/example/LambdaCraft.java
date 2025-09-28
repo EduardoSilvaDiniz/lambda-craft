fed7306043793804fde51253892ec9acb5f2e137
Eduardo Silva <eduardofox989@protonmail.com>
2025-09-27 22:55:10 -0300

    added: registry and initialize sandvich and sounds

--- src/main/java/com/example/LambdaCraft.java
+++ src/main/java/com/example/LambdaCraft.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,12 +9,13 @@ import net.fabricmc.api.ModInitializer;
 
 
 public class LambdaCraft implements ModInitializer {
-  public static final String MOD_ID = "lambda-craft";
-  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "lambda-craft";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-  @Override
-  public void onInitialize() {
-		ModItems.initialize();
-    LOGGER.info("Hello Fabric world!");
-  }
+    @Override
+    public void onInitialize() {
+        ModItems.initialize();
+        CustomSounds.initialize();
+        LOGGER.info("Hello Fabric world!");
+    }
 }
--- src/main/java/com/example/ModItems.java
+++ src/main/java/com/example/ModItems.java
@@ -11,53 +11,46 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
-import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-  public static final Item CROWBAR =
-      ModItems.register(
-          "crowbar", Item::new, new Item.Settings().sword(ToolMaterial.IRON, 5f, 2f));
+    public static final Item CROWBAR = ModItems.register("crowbar", Item::new, new Item.Settings().sword(ToolMaterial.IRON, 5f, 2f));
 
-  public static final ConsumableComponent POISON_FOOD_CONSUMABLE_COMPONENT =
-      ConsumableComponents.food()
-          .consumeEffect(
-              new ApplyEffectsConsumeEffect(
-                  new StatusEffectInstance(StatusEffects.SATURATION, 6 * 20, 1), 1.0f))
-          .build();
+    public static final ConsumableComponent POISON_FOOD_CONSUMABLE_COMPONENT = ConsumableComponents.food().consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.SATURATION, 6 * 20, 1), 1.0f)).build();
 
-  public static final FoodComponent POISON_FOOD_COMPONENT =
-      new FoodComponent.Builder().alwaysEdible().build();
+    public static final FoodComponent POISON_FOOD_COMPONENT = new FoodComponent.Builder().alwaysEdible().build();
 
-  public static final Item SANDVICH =
-      ModItems.register(
-          "sandvich",
-          Item::new,
-          new Item.Settings().food(POISON_FOOD_COMPONENT, POISON_FOOD_CONSUMABLE_COMPONENT));
+    public static final String SANDVICH_ID = "sandvich";
 
-  public static Item register(
-      String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+    public static final Identifier SANDVICH_ID_IDENTIFIER = Identifier.of(LambdaCraft.MOD_ID, SANDVICH_ID);
 
-    RegistryKey<Item> itemKey =
-        RegistryKey.of(RegistryKeys.ITEM, Identifier.of(LambdaCraft.MOD_ID, name));
+    public static final Item SANDVICH = ModItems.register("sandvich", Sandvich::new, new Item.Settings().food(POISON_FOOD_COMPONENT, POISON_FOOD_CONSUMABLE_COMPONENT));
 
-    Item item = itemFactory.apply(settings.registryKey(itemKey));
-    Registry.register(Registries.ITEM, itemKey, item);
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
 
-    return item;
-  }
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(LambdaCraft.MOD_ID, name));
 
-  public static void initialize() {
-    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
-        .register(itemGroup -> itemGroup.add(CROWBAR));
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+        Registry.register(Registries.ITEM, itemKey, item);
 
-    ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
-        .register(itemGroup -> itemGroup.add(SANDVICH));
-  }
+        return item;
+    }
+
+    public static Item register(Identifier id, Item item) {
+        LambdaCraft.LOGGER.info("Tentando Registrar o sandvich!!!!!!!!!!!");
+        return Registry.register(Registries.ITEM, id, item);
+    }
+
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(itemGroup -> itemGroup.add(CROWBAR));
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
+                .register(itemGroup -> itemGroup.add(SANDVICH));
+    }
 }
